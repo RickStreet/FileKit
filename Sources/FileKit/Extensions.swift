b5f813bc6fa28de21edef1cebe0a51e6cfed04a0
RickStreet <Rick_1@me.com>
2022-02-02 16:51:10 -0800

    Added Aspen file types

--- Sources/FileKit/Extensions.swift
+++ Sources/FileKit/Extensions.swift
@@ -10,8 +10,17 @@ import UniformTypeIdentifiers
 
 @available(macOS 11.0, *)
 public extension UTType {
-    //Word documents are not an existing property on UTType
     static var vls: UTType {
         UTType.types(tag: "vls", tagClass: .filenameExtension, conformingTo: nil).first!
     }
+    static var ccf: UTType {
+        UTType.types(tag: "ccf", tagClass: .filenameExtension, conformingTo: nil).first!
+    }
+    static var mdl: UTType {
+        UTType.types(tag: "mdl", tagClass: .filenameExtension, conformingTo: nil).first!
+    }
+    static var dpa: UTType {
+        UTType.types(tag: "dpa", tagClass: .filenameExtension, conformingTo: nil).first!
+    }
+
 }
