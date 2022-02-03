b031cd5aed9d8f1bfd9a14a7a1ed77bdf7419566
RickStreet <Rick_1@me.com>
2022-02-02 16:35:11 -0800

    Added .vls content type

---
+++ Sources/FileKit/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  File 2.swift
+//  
+//
+//  Created by Rick Street on 2/2/22.
+//
+
+import Foundation
+import UniformTypeIdentifiers
+
+@available(macOS 11.0, *)
+extension UTType {
+    //Word documents are not an existing property on UTType
+    static var vls: UTType {
+        UTType.types(tag: "vls", tagClass: .filenameExtension, conformingTo: nil).first!
+    }
+}
--- Sources/FileKit/File.swift
+++ Sources/FileKit/File.swift
@@ -10,12 +10,12 @@ import Cocoa
 import UniformTypeIdentifiers
 
 // class must be public for package to work
-@available(OSX 11.0, *)
+@available(macOS 11.0, *)
 public class File {
     public var title = "Read Title"
     public var message = "Test save message"
     // public var allowedFileTypes: [String]? = nil
-    public var allowedContentTypes: [UTType] = [.plainText]
+    public var allowedContentTypes: [UTType] = [.vls]
     public var canCreateDirectories = true
     public var nameFieldLabel = "Save as:"
     public var useDefaultURL = true // Set false if you don't want to save last file opened
